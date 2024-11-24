8aea8044b007e0d4d04c4b5ba12a9534e59d20e5
Diego Diaz <diegoa.diazr@konradlorenz.edu.co>
2024-11-24 17:32:44 -0500

    Rutas en app.routes.ts para registro admin/usuario y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { InventarioComponent } from './inventario/inventario.component';
 import { HomeComponent } from './home/home.component';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { RegistroAdminComponent } from './registro-admin/registro-admin.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Ruta para el Home
-  { path: 'inventario', component: InventarioComponent } // Ruta para Inventario
+  { path: 'inventario', component: InventarioComponent }, // Ruta para Inventario
+  { path: 'signup', component: RegistroUsuarioComponent }, // Ruta para registro de Usuario
+  { path: 'signupadmin', component: RegistroAdminComponent }, // Ruta para registro de Admin
+  { path: 'login', component: LoginComponent } // Ruta para Login
 ];
