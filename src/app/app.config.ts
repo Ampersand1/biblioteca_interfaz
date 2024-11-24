e10335072e2de727c0b9097e7d8d4a123d840e37
fsalamanca2000 <juanf.salamancag@konradlorenz.edu.co>
2024-11-24 17:08:35 -0500

    Agregar los metodos put, post y delete mas añadir detalles ruta a ruta

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';  // Importa provideHttpClient
+import { provideHttpClient } from '@angular/common/http'; // Asegúrate de importar provideHttpClient
 import { routes } from './app.routes';
 import { LibroService } from './services/libro.service';  // Asegúrate de importar tu servicio
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes),   // Asegúrate de que las rutas estén siendo proporcionadas
     provideHttpClient(),  // Añade el provideHttpClient aquí
     LibroService  // Asegúrate de incluir tu servicio en los proveedores
   ]
