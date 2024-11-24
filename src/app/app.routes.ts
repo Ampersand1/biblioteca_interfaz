82c81f9898225aaf4ae3dfdd7223053430e5b794
fsalamanca2000 <juanf.salamancag@konradlorenz.edu.co>
2024-11-24 14:15:20 -0500

    Cambio a la estructura de rutas y creacion componente home e inventario
    Optimizar la estructura para poder trabajar con las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InventarioComponent } from './inventario/inventario.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta para el Home
+  { path: 'inventario', component: InventarioComponent } // Ruta para Inventario
+];
---
+++ src/app/inventario/inventario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventario',
+  standalone: true,
+  templateUrl: './inventario.component.html',
+  styleUrls: ['./inventario.component.css']
+})
+export class InventarioComponent {
+  title = 'biblioteca_UI';
+}
