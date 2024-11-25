248e55de1d3adcc911a5a2b2a8a23499265ea860
Diego Diaz <diegoa.diazr@konradlorenz.edu.co>
2024-11-24 19:58:57 -0500

    metodo redirectToHome para login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
 import { AutenticacionService } from '../services/autenticacion.service'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [FormsModule] 
+  imports: [FormsModule]
 })
 export class LoginComponent {
+  
+  constructor(
+    private autenticacionService: AutenticacionService,
+    private router: Router  // Inyectar Router para la navegación
+  ) {}
 
-  constructor(private autenticacionService: AutenticacionService) { }
+  // Función para redirigir al Home
+  redirectToHome() {
+    this.router.navigate(['/']);  // Redirige al Home
+  }
 
+  // Función de inicio de sesión
   onSubmit(loginData: any) {
     this.autenticacionService.login(loginData).subscribe(
       (response) => {
-        console.log('Inicio de sesión exitoso', response);
-        // Puedes redirigir al usuario a otra página si es necesario
+        console.log('Sesión iniciada con éxito', response);
+        this.redirectToHome();  // Redirige al Home después de un inicio de sesión exitoso
       },
       (error) => {
         console.error('Error al iniciar sesión', error);
-        alert('Credenciales incorrectas');
+        alert('Credenciales incorrectas o error en el inicio de sesión.');
       }
     );
   }
