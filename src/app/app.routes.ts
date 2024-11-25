95392f4d82ec8fd60f92ba1c901e56dad06681e0
fsalamanca2000 <juanf.salamancag@konradlorenz.edu.co>
2024-11-24 22:13:09 -0500

    Commit bendito v3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 import { RegistroAdminComponent } from './registro-admin/registro-admin.component';
 import { LoginComponent } from './login/login.component';
-import { ReservasComponent } from './reservas/reservas.component'; 
+import { ReservasComponent } from './reservas/reservas.component'; // Importar el componente de reservas
+import { AuthGuard } from './auth.guard';  // Importar el guard
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Ruta para el Home
-  { path: 'inventario', component: InventarioComponent }, // Ruta para Inventario
+  { path: 'inventario', component: InventarioComponent, canActivate: [AuthGuard] }, // Ruta protegida
   { path: 'signup', component: RegistroUsuarioComponent }, // Ruta para registro de Usuario
   { path: 'signupadmin', component: RegistroAdminComponent }, // Ruta para registro de Admin
   { path: 'login', component: LoginComponent }, // Ruta para Login
-  { path: 'reservas', component: ReservasComponent } // Ruta para reservas
+  { path: 'reservas', component: ReservasComponent, canActivate: [AuthGuard] }, // Ruta protegida
 ];
